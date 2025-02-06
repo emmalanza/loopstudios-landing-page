3f34652ce02423878cb74f839eb9a65546683d6c
Emma <emmalanza@hotmail.com>
2025-02-06 16:13:37 +0100

    adding workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site:"https://emmalanza.github.io",
+    base:"loopstudios-landing-page",
     vite: {
         plugins: [tailwindcss()],
       },
